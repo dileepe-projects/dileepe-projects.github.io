847cc782a86e333879259dd7dd95ce2776fc4b14
Ekambaram <DEkambaram@DIRECTV.com>
2016-08-12 18:12:22 +0530

    updated

---
+++ js/todo-controller.js
@@ -0,0 +1,34 @@
+var todo = angular.module('todoapp', []);
+
+
+todo.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+
+todo.controller('mainController', function($scope){
+
+	$scope.todoitems = [];
+
+	$scope.addTodoItem = function(){
+		var myDate = new Date();
+		var text = $scope.todotext;
+		var age = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+
+		$scope.todoitems.push({"text": text, "age": age});
+
+	};
+
+  $scope.orderList = "age";
+
+});
