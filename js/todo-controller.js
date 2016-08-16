eb3f77d788d9391355e6f5089aa4b39a1a95eb68
dileepApps <dilipk26@gmail.com>
2016-08-16 12:38:32 +0530

    Add files via upload
    removed on enter change (facing issues in mobile browser)

--- js/todo-controller.js
+++ js/todo-controller.js
@@ -21,11 +21,16 @@ todo.controller('mainController', function($scope){
 	$scope.todoitems = [];
 
 	$scope.addTodoItem = function(){
+        if($scope.todotext == undefined)
+        {
+            //alert("Empty Todo, enter a value!");
+        }
+        else {
 		var myDate = new Date();
 		var text = $scope.todotext;
-		var age = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
-
-		$scope.todoitems.push({"text": text, "age": age});
+		var age = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();		
+        $scope.todoitems.push({"text": text, "age": age});
+    }
 
 	};
 
