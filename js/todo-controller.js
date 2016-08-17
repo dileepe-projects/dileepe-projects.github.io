e68a6bbb425812a59520e1893fb84829acc818b7
dileepApps <dilipk26@gmail.com>
2016-08-17 19:18:45 +0530

    Add files via upload

--- js/todo-controller.js
+++ js/todo-controller.js
@@ -1,129 +1,131 @@
-var todo = angular.module('todoapp', []);
+var todoapp = angular.module('todoapp', []);
 
 
+todoapp.controller('todoController', function($scope, $window, dateFilter, $interval) {
+var promise;
 
-todo.controller('mainController', function($scope, $interval, dateFilter){
-
-    
-
-    var format = "M/d/yy, h:mm:ss a"; //format for reminder date and time
-
-    var target = "8/16/16, 4:22:23 PM";
-
-    
-
-    var promise; //promise that returns interval
-
-    //check if the browser supports notifications
-    if (!("Notification" in window)) {
-    $scope.desktopNotification = "Not Supported";
-    $scope.labelClass = "label label-danger";
+//check if desktop notification is available in the current browser
+	if (!("Notification" in window)) {
+	    $scope.desktopNotification = "Not Supported";
+	    $scope.iconClass = "glyphicon glyphicon-remove red";	    
     }
 
     else if(Notification.permission === "granted") {
-    $scope.desktopNotification = Notification.permission;
-    $scope.labelClass = "label label-success";
+    $scope.desktopNotification = "Available";
+    $scope.iconClass = "glyphicon glyphicon-ok green";
     }
 
     else if(Notification.permission === "denied") {
-    $scope.desktopNotification = Notification.permission;
-    $scope.labelClass = "label label-danger";
+    $scope.desktopNotification = "Denied (by user)";
+    $scope.iconClass = "glyphicon glyphicon-exclamation-sign orange";
     }
 
     else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
-     
-      if (permission === "granted") {
-        $scope.desktopNotification = Notification.permission;
-        $scope.labelClass = "label label-success";
-      }
+		      $window.location.reload(); //reload to see what the user has selected (this is needed in chrome)
+		      if (permission === "granted") {
+		       $scope.desktopNotification = "Available";
+		       $scope.iconClass = "glyphicon glyphicon-ok green";
+		      }
 
-      if (permission === "denied") {
-         $scope.desktopNotification = Notification.permission;
-         $scope.labelClass = "label label-danger";
-      }
-
+		      if (permission === "denied") {
+		         $scope.desktopNotification = "Denied (by user)";
+		    	 $scope.iconClass = "glyphicon glyphicon-exclamation-sign orange";
+		      }
     });
     }
 
-    function updateTime() {
-        console.log(dateFilter(new Date(), format));
-        if(dateFilter(new Date(), format) == target)
+
+
+    $scope.todoList = [];
+
+    
+
+
+    $scope.todoAdd = function() {
+        if($scope.todoInput == undefined||$scope.todoInput == "")
         {
-            spawnNotification('Hi There!', 'images/todo.png', 'TODO App says');
-            //var notification = new Notification("Hi there!");
-        }
 
-    }
-
-    //promise =  $interval(updateTime, 1000);
-
-    $scope.stopInterval = function(){
-        $interval.cancel(promise);
-     }
-        
-	$scope.todoitems = [];
-
-	$scope.addTodoItem = function(){
-        if($scope.todotext == undefined)
-        {
-            //alert("Empty Todo, enter a value!");
         }
         else {
-		//var myDate = new Date();
-		var text = $scope.todotext;
-		//var age = myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();		
-        var age= dateFilter(new Date(), format);
-        $scope.todoitems.push({"text": text, "age": age, "reminder": "NA"});
-        $scope.todotext = "";
+        $scope.todoList.push({todoText:$scope.todoInput, todoReminder:'NA'});        
+        $scope.todoInput = "";
+    	}
+    };    
+
+    $scope.removetask = function($index){    
+    $scope.todoList.splice($index,1);  
+    };
+
+
+    $scope.passReminder = function($index){
+    	
+    	$scope.currentSNO = $index;    	
     }
 
-	};
-
-  $scope.orderList = "age";
-
-  function spawnNotification(theBody, theIcon, theTitle) {
-  var options = {
-    body: theBody,
-    icon: theIcon
-  }
-  var n = new Notification(theTitle, options);  
+    $scope.addReminder = function(){
+    	
+    	$scope.todoList[$scope.currentSNO].todoReminder = $scope.selectedTime;  
+    	$scope.closemodal = "modal";    	
     }
 
+
+
+
+    
     var now = new Date();
-    now.setMinutes(now.getMinutes() + 5);
-
+    now.setMinutes(now.getMinutes() + 3); //fiveminutes interval to set the reminder
     var tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
     tomorrow.setHours(0,0,0,0);
-    
     $scope.availableOptions = [];
-
-    
-    console.log(tomorrow);
-
-    
-
-    while( now < tomorrow ) {
-
-      
-
-      //console.log(dateFilter(now, 'h:mm a'));
-
-      $scope.availableOptions.push({id: dateFilter(now, 'h:mm a'), name: dateFilter(now, 'h:mm a')})
-          
-      now.setMinutes(now.getMinutes() + 5);
+    while( now < tomorrow ) {      
+      $scope.availableOptions.push({id: dateFilter(now, 'h:mm a'), name: dateFilter(now, 'h:mm a')})          
+      now.setMinutes(now.getMinutes() + 3); //fiveminutes interval to set the reminder (change here also)
     }
 
+    $scope.selectedTime = $scope.availableOptions[0].name;
 
-    $scope.setreminder = function(){
-
-      console.log($scope.selectedTime);
+  	var found = false;
 
 
+    function checkTime() {
+        //console.log($scope.todoList.length);   
+         
+         if(!found && $scope.todoList.length > 0)
+         {    
+         		console.log("found: " + found);
+		        console.log("inside the function");
+
+		        for(var i=0; i<$scope.todoList.length; i++)
+		        {
+
+		        	if(dateFilter(new Date(), 'h:mm a') == $scope.todoList[i].todoReminder)
+			        {
+			            console.log("found match at: " + i + ", on " + $scope.todoList[i].todoReminder);
+			           	//spawnNotification($scope.todoList[i].todoText, 'images/todo.png', 'TODO App says');
+			          	found = true;	
+
+			            
+			        }			       
+
+
+		        }
+
+		 }
 
     }
 
-  
+    promise =  $interval(checkTime, 5000);
+     
+    function spawnNotification(theBody, theIcon, theTitle) {
+		  var options = {
+		    body: theBody,
+		    icon: theIcon
+		  }
+  		var n = new Notification(theTitle, options);  
+    }
+
 
 });
+
