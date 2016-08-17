05826e031d1685f3d13ba653074f179ce6e69cc9
Dileep <dileep.ekambaram@live.com>
2016-08-17 22:51:54 +0530

    removed test

--- js/main.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
- console.log('Service Worker is supported');
- navigator.serviceWorker.register('./js/sw.js').then(function(reg) {
-   console.log(':^)', reg);
-   // TODO
- }).catch(function(err) {
-   console.log(':^(', err);
- });
-}
