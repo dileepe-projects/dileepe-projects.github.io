8a286368f79eb85870fccaf7abd9e8d6608a1bbd
Dileep <dilipk26@gmail.com>
2019-02-01 22:06:36 +0530

    Update expenses.js

--- js/expenses.js
+++ js/expenses.js
@@ -1,4 +1,4 @@
-$.getJSON( "data.json", function( data ) {
+$.getJSON( "./data/data.json", function( data ) {
   var items = [];
   $.each( data, function( key, val ) {
     items.push( "<li id='" + key + "'>" + val + "</li>" );
